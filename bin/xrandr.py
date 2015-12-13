69dabef90153d1b74ba91ff9fab12f6a5e8d5fe5
Mario Manno <mail@manno.name>
2015-12-13 12:25:59 +0100

    executables paths

--- bin/xrandr.py
+++ bin/xrandr.py
@@ -20,14 +20,14 @@ class actions:
         self.status.set_label("running xrandr")
         os.system("xrandr --fb 1366x768 --output HDMI2 --off --output HDMI3 --off --output DP2 --off   --output LVDS1 --auto")
         os.system("~/.config/i3/bin/compton reset")
-        os.system("awsetbg ~/.local/share/wallpaper.png")
+        os.system("~/.config/i3/awsetbg ~/.local/share/wallpaper.png")
         gtk.main_quit()
 
     def xrandr_work_action(self,btn):
         self.status.set_label("running xrandr")
         os.system("xrandr --output LVDS1 --off --output DP2 --left-of HDMI3 --auto --output HDMI3 --mode 1680x1050")
         os.system("~/.config/i3/bin/compton reset")
-        os.system("awsetbg ~/.local/share/wallpaper.png")
+        os.system("~/.config/i3/awsetbg ~/.local/share/wallpaper.png")
         #os.system("i3-msg 'output HDMI3; append_layout ~/.i3/layouts/work-HDMI3'");
         #os.system("i3-msg 'output DP2; append_layout ~/.i3/layouts/work-DP2'");
         gtk.main_quit()
@@ -36,7 +36,7 @@ class actions:
         self.status.set_label("running xrandr")
         os.system("xrandr --output LVDS1 --off --output HDMI3 --auto --output HDMI2 --left-of HDMI3 --auto")
         os.system("~/.config/i3/bin/compton reset")
-        os.system("awsetbg ~/.local/share/wallpaper.png")
+        os.system("~/.config/i3/awsetbg ~/.local/share/wallpaper.png")
         gtk.main_quit()
 
     def add_button(self, title, action):
